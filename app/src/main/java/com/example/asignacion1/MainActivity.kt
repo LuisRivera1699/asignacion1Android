d2d6de168ee7124afe6014de5c95daf63cfd9e12
Luis Rivera <luisriveradiaz99@gmail.com>
2020-05-01 12:51:18 -0500

    class randomizer

--- app/src/main/java/com/example/asignacion1/MainActivity.kt
+++ app/src/main/java/com/example/asignacion1/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val intarr : IntArray = intArrayOf(1,2,3,4,5,6,7,8)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/asignacion1/Randomizer.kt
@@ -0,0 +1,9 @@
+package com.example.asignacion1
+
+class Randomizer {
+
+    fun getRandomNumbers(intarr : IntArray, q : Int) : IntArray{
+        var shuffled : IntArray = intarr
+    }
+
+}
