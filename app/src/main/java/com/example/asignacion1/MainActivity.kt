3c758a357e22a8e47cd5b2cfdb7c8d73439a7306
Luis Rivera <luisriveradiaz99@gmail.com>
2020-05-02 02:45:38 -0500

    completed

--- app/src/main/java/com/example/asignacion1/MainActivity.kt
+++ app/src/main/java/com/example/asignacion1/MainActivity.kt
@@ -10,55 +10,62 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
-    val intarr : IntArray = intArrayOf(1,2,3,4,5,6,7,8,9,10,11,12)
+    // Array of emojis unicodes
+    var intarr : IntArray? = null
 
-    var emojiArray : Array<String>? = null
-
-
+    // Layout views
     var buttons : Array<Button>? = null
     var teVi : TextView? = null
 
+    // Int array for storing buttons.size/2 numbers of shuffled emojis
     var couplesArray : IntArray? = null
 
+    // Current couples
     var firstCoupleElement : Button? = null
     var secondCoupleElement : Button? = null
 
+    // Game process variables
     var attempts : Int = 0
     var goodMoves : Int = 0
     var won : Boolean = false
 
+    // Random for shuffling and generating random emojis array
     val rnd = Randomizer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get text view from layout and set text to "Turno Jugador"
         teVi = findViewById(R.id.tevi1)
         teVi!!.text = "Turno Jugador"
 
-        emojiArray = arrayOf(
-            getEmoji(0x1F60A),
-            getEmoji(0x1F4A9),
-            getEmoji(0x1F921),
-            getEmoji(0x1F479),
-            getEmoji(0x1F47B),
-            getEmoji(0x1F47D),
-            getEmoji(0x1F47E),
-            getEmoji(0x1F916),
-            getEmoji(0x1F64A),
-            getEmoji(0x1F4A5),
-            getEmoji(0x1F573),
-            getEmoji(0x1F4A3),
-            getEmoji(0x1F4AC),
-            getEmoji(0x1F4AA),
-            getEmoji(0x1F385),
-            getEmoji(0x1F9DC),
-            getEmoji(0x1F9DE),
-            getEmoji(0x1F9DF),
-            getEmoji(0x1F3C3),
-            getEmoji(0x1F483),
-            getEmoji(0x1F93A)
+        // Fill intarr with emojis unicode values
+        intarr = intArrayOf(
+            0x1F60A,
+            0x1F4A9,
+            0x1F921,
+            0x1F479,
+            0x1F47B,
+            0x1F47D,
+            0x1F47E,
+            0x1F916,
+            0x1F64A,
+            0x1F4A5,
+            0x1F573,
+            0x1F4A3,
+            0x1F4AC,
+            0x1F4AA,
+            0x1F385,
+            0x1F9DC,
+            0x1F9DE,
+            0x1F9DF,
+            0x1F3C3,
+            0x1F483,
+            0x1F93A
         )
 
+        // fill buttons array with buttons from the layout
         buttons = arrayOf(
             findViewById(R.id.but1),
             findViewById(R.id.but2),
@@ -74,76 +81,113 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.but12)
         )
 
-        couplesArray = rnd.makeArrayOfCouples(intarr, buttons!!.size/2)
-        Log.i("Asignacion 1", "true")
+        // Make a emoji couples array with shuffled emojis from the emojis unicode values array
+        couplesArray = rnd.makeArrayOfCouples(intarr!!, buttons!!.size/2)
 
+        //Log.i("Asignacion 1", "true")
+
+        // Initializate buttons behaviour
         for (i in buttons!!){
+            // Characteristics
             i.isClickable = true
+            i.textSize = 40.03F
+
+            // OnClickListener with condition
             i.setOnClickListener {
+                // If hasn't won yet, when clicked, it text will be placed with emoji
                 if (!won){
+                    // Get button index
                     val buttonIndex = buttons!!.indexOf(i)
-                    i.text = couplesArray!![buttonIndex].toString()
+                    // Set button text to emoji by processing the unicode value
+                    i.text = String(Character.toChars(couplesArray!![buttonIndex]))
+                    // Set the current couple element
                     setCurrentCoupleElement(i)
-                }else{
-                    // Reiniciar app
+                }
+                // If won, when clicked, the activity will be recreated
+                else{
+                    // Reload app
                     this.recreate()
                 }
             }
         }
+
     }
 
     fun setCurrentCoupleElement(clickedButton : Button){
-
+        // If the first couple element is null, it will be filled with the button clicked
         if (firstCoupleElement == null){
             firstCoupleElement = clickedButton
-        } else{
+        }
+        // Otherwise, the secondCoupleElement will be filled and checked if the two elements match
+        else{
             secondCoupleElement = clickedButton
-            checkCouple()
+            // Check for match
+            checkForMatch()
         }
 
     }
 
-    fun checkCouple(){
-        if (firstCoupleElement!!.text == secondCoupleElement!!.text){
-            attempts += 1
+    fun checkForMatch(){
 
+        // If they match
+        if (firstCoupleElement!!.text == secondCoupleElement!!.text){
+            // Number of attempts and goodMoves will be incremented in one
+            attempts += 1
+            goodMoves += 1
+            // TextView text set to  "Turno Jugador"
+            teVi!!.text = "Turno Jugador"
+
+            // Current couple elements won't be clickable anymore
             firstCoupleElement!!.isClickable = false
             secondCoupleElement!!. isClickable = false
 
+            // Current couple elements set back to null
             firstCoupleElement = null
             secondCoupleElement = null
 
-            goodMoves += 1
-            teVi!!.text = "Turno Jugador"
-
+            // Check for win
             checkForWin()
-        }else {
+
+        }
+        // Otherwise
+        else {
+            // Current couple elements text will be set back to ""
             firstCoupleElement!!.text = ""
             secondCoupleElement!!.text = ""
 
+            // Current couple elements set back to null
             firstCoupleElement = null
             secondCoupleElement = null
 
+            // Number of attempts incremented by 1
             attempts += 1
+
+            // Text View set to "Error"
             teVi!!.text = "Error"
         }
-        Log.i("Asignacion 1", attempts.toString())
+        //Log.i("Asignacion 1", attempts.toString())
     }
 
+    // Check for win
     fun checkForWin(){
+        // If the good moves reaches to buttons array size divided by 2
         if (goodMoves == buttons!!.size/2){
+            // won variable will be set to true
             won = true
+
+            // Text View will be set to "Fin, lo terminaste en x intentos"
             teVi!!.text = "Fin, lo terminaste en ${attempts.toString()} intentos"
+
+            // Buttons will be clickable again, as the game is won, it will be able to reload when clicked
             for (i in buttons!!){
                 i.isClickable = true
             }
-        }else{
 
         }
-    }
-
-    fun getEmoji(unicode: Int): String {
-        return String(Character.toChars(unicode))
+        // Otherwise it wont do nothing
+        else{
+            // If only it was a "pass" like in python or java.
+        }
     }
 
 }
